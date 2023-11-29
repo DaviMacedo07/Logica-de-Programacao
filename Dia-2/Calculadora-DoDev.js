b8d87f6af4dd36983fe4410beedd67142d518ca2
Davi Macêdo <dvmacedo14@gmail.com>
2023-11-29 13:28:59 -0300

    Calculadora resolvida

--- Dia-2/Calculadora-DoDev.js
+++ Dia-2/Calculadora-DoDev.js
@@ -1,10 +1,10 @@
-function Calculadora (numero1 , numero2 , operador) {
-    let resultado
+
+function Calculadora (numero1 , numero2 , operador ) {
+    
     let num1 = Number(numero1)
     let num2 = Number(numero2)
-    num1 = Number(prompt("DIgite um numero"))
-    num2 = Number(prompt("DIgite outro numero"))
-    window.prompt("Selecione sua operação matemática :" + "\n1 = Soma ; \n2 = Subtração ; \n3 = Multiplicação ; \n4 = Divisão")  
+    
+    let resultado
 
     switch(operador) {
         case "1" :
@@ -16,27 +16,36 @@ function Calculadora (numero1 , numero2 , operador) {
         break;
 
         case "3" :
-        resultado = num1 / num2;
+        resultado = num1 * num2;
         break;
 
         case "4" :
-        resultado = num1 * num2;
+        resultado = num1 / num2;
         break;
 
         case "5" :
         let contador = 1
-        resultado = num1 * num2
-        while( contador < num2 - num1) {
+        resultado = num1
+        while( contador < num2 ) {
             resultado = resultado * num1 
             contador++
         }
         break;
+     default: 
+        resultado = "Operador inválido"
+        break;
     }
 
-        if (resultado == undefined || resultado > 1000000) 
-            resultado = "ERRO"
+        if (isNaN(resultado) || resultado > 1000000) 
+            resultado = "ERRO";
 
     return resultado
+
+    
 }
 
-console.log(Calculadora())
+let num1Input = prompt("Digite o primeiro número a ser calculado")
+let num2Input = prompt("Digite o segundo número a ser calculado")
+let operadorInput = prompt("Selecione sua operação matemática : \n1 = Soma; \n2 = Subtração; \n3 = Multiplicação; \n4 = Divisão; \n5 = Potenciação")
+
+console.log(Calculadora(num1Input, num2Input , operadorInput))
