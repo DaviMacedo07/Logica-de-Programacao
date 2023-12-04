a47e137dccd1cbec71d16743f3d6ca10ab2b1d03
Davi Macêdo <dvmacedo14@gmail.com>
2023-12-04 08:28:17 -0300

    iniciando exercício 1.3

---
+++ Dia-4/Lista-Array-1.3.js
@@ -0,0 +1,7 @@
+let array = []
+let arrayInvertido = []
+let quantidade = parseInt(prompt("Digite a quantidade de números a serem inseridos no array"))
+
+for (let i = 0 ; i < quantidade ; i++) {
+    let numero = parseInt(prompt(""))
+}
